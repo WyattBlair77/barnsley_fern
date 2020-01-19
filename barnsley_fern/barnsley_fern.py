89ebeae5bbc0318c98a54e5e220a2e12727b089b
WyattBlair77 <42503373+WyattBlair77@users.noreply.github.com>
2020-01-19 11:11:53 -0800

    Update barnsley_fern.py

--- barnsley_fern/barnsley_fern.py
+++ barnsley_fern/barnsley_fern.py
@@ -9,6 +9,23 @@ pen.penup()
 x = 0
 y = 0
 
+hyper_params = [0.16,
+                0.85,
+                0.04,
+                0.04,
+                0.85,
+                1.6,
+                0.20,
+                0.26,
+                0.23,
+                0.22,
+                1.6,
+                0.15,
+                0.28,
+                0.26,
+                0.24,
+                0.44]
+
 for n in range(100000):
     pen.goto(85*x, 57*y-275)  # 57 is to scale the fern and -275 is to start the drawing from the bottom.
     pen.pendown()
@@ -20,15 +37,14 @@ for n in range(100000):
     yn = y
     if r < 1:  # elif ladder based on the probability
         x = 0
-        y = 0.16 * yn
+        y = hyper_params[0] * yn
     elif r < 86:
-        x = 0.85 * xn + 0.04 * yn
-        y = -0.04 * xn + 0.85 * yn + 1.6
+        x = hyper_params[1] * xn + hyper_params[2] * yn
+        y = -hyper_params[3] * xn + hyper_params[4] * yn + hyper_params[5]
     elif r < 93:
-        x = 0.20 * xn - 0.26 * yn
-        y = 0.23 * xn + 0.22 * yn + 1.6
+        x = hyper_params[6] * xn - hyper_params[7] * yn
+        y = hyper_params[8] * xn + hyper_params[9] * yn + hyper_params[10]
     else:
-        x = -0.15 * xn + 0.28 * yn
-        y = 0.26 * xn + 0.24 * yn + 0.44
+        x = -hyper_params[11] * xn + hyper_params[12] * yn
+        y = hyper_params[13] * xn + hyper_params[14] * yn + hyper_params[15]
 
-        
